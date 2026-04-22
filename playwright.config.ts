6a4383cc9e83b2eacd147df4d4649b2644198901
Kietntse173576 <boibitcute@gmail.com>
2026-04-22 16:15:21 +0700

    fix bug:worker in rating

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   timeout: 90000,
   testDir: './tests',
+  grep: process.env.CI ? /^(?!.*@local).*$/ : undefined,
   
   /* Run tests in files in parallel */
   fullyParallel: true,
